ab325eae617c379c62da4a6f198eab76c4b203d7
Pranjal chugh <Pranjal@Pranjal-PC.(none)>
2012-01-21 13:15:47 +0530

    Yet another game

---
+++ game.cpp
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    while(a)
+{             scanf("%d",&b);
+              if(b%2)
+                          printf("BOB\n");
+              else
+                          printf("ALICE\n");
+             a--;
+}
+  
+    return 0;
+}    
+    
+                      
+                                          
